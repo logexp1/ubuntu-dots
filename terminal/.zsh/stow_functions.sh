9b5c670f5aea454058cd06aea2d2ea8824dbcedf
Jisoo Lee <jisoo.h.lee@gmail.com>
2025-03-19 16:15:45 +0900

    minor fix

--- terminal/.zsh/stow_functions.sh
+++ terminal/.zsh/stow_functions.sh
@@ -1,10 +1,10 @@
 # as of stow 2.2.2, READMEs are automatically ignored
 restow() {
 	cd ~/ubuntu-dots && \
-		stow -Rvt ~/ emacs terminal wm
+		stow -Rvt ~/ emacs terminal wm remap
 }
 
 unstow() {
 	cd ~/mydots && \
-		stow -Dvt ~/ emacs terminal wm
+		stow -Dvt ~/ emacs terminal wm remap
 }
