4be4826cb8268b3d47f90e0f497b2e481439f694
Jisoo Lee <jisoo.h.lee@gmail.com>
2026-04-12 13:49:02 +0900

    update system

--- install/wal.sh
+++ install/wal.sh
@@ -3,7 +3,23 @@ set -e
 source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/common.sh"
 
 run() {
-    wal --theme base16-default -s -t
+    local wallpapers_src="$BASEDIR/assets/wallpapers"
+
+    if [[ ! -d "$wallpapers_src" ]]; then
+        log_error "wal" "Wallpapers directory not found at $wallpapers_src"
+        return 1
+    fi
+
+    # Symlink the whole wallpapers directory
+    [[ -d "$HOME/.wallpapers" && ! -L "$HOME/.wallpapers" ]] && rm -rf "$HOME/.wallpapers"
+    ln -sf "$wallpapers_src" "$HOME/.wallpapers"
+
+    # Pick a random wallpaper excluding lock-wallpaper.png
+    local wallpaper
+    wallpaper=$(find "$HOME/.wallpapers" -type f ! -name 'lock-wallpaper.png' | shuf -n 1)
+
+    log_step "wal" "Generating color scheme from $wallpaper..."
+    wal -i "$wallpaper" -s -t
     log_step "wal" "Done."
 }
 
